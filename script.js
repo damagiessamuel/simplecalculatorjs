5242a69e1d0875a9cd5e1dee99bcc2c4205f2143
Chukwunonso Samuel Enuoyibo <105329788+damagiessamuel@users.noreply.github.com>
2022-06-21 06:19:39 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+const operator = prompt('Abundant Minute Operator: Type in an oepration to start a calculation ( either +, -, * or / ): ');  
+  
+
+const number1 = parseFloat(prompt ('Type the first number for SmallSamuels calculator: '));  
+const number2 = parseFloat(prompt ('Type the Second number for SmallSamuels calculator: '));  
+  
+let result; 
+  
+
+if (operator == '+') { 
+    result = number1 + number2;  
+}  
+else if (operator == '-') {
+    result = number1 - number2;  
+}  
+else if (operator == '*') { 
+    result = number1 * number2;  
+}  
+else {  
+    result = number1 / number2; 
+}  
+  
+  
+window.alert(" This is the result on SmallSamuel's Calculator Result is" + result);  
